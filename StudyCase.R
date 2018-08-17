41a5226936dca54f2b18f5bcea9fe594bbcd6b57
CristhianRivera <cristhianr.jimenez.rivera@gmail.com>
2018-08-17 14:12:39 +0200

    weekly datetime added

--- StudyCase.R
+++ StudyCase.R
@@ -40,7 +40,7 @@ path = '/Users/Cristhian/Documents/Metro/case_study/'
 data_metro <- read.csv(file= paste(path,"data.csv", sep = ""),
                          header = TRUE, 
                          sep=",")
-
+View(data_metro)
 distinct(data_metro,calendar_date)
 data_metro$calendar_date <- lubridate::mdy(data_metro$calendar_date)
 
@@ -205,8 +205,10 @@ mape(fore,ori)
 
 
 ################### 2 #################################
+##################Per product_id######################
 
 data_metro_eda <- data_metro %>%
+  #mutate(calendar_date = woy) %>%
   group_by(calendar_date, product_id) %>%
   summarise(volume_sold = sum(volume_sold),
             revenue = sum(revenue),
@@ -215,56 +217,165 @@ data_metro_eda <- data_metro %>%
             retail_price = sum(retail_price),
             mpdm1 = sum(mpdm1),
             cdm1  = sum(cdm1) )%>%
-  filter(product_id==13701) %>%
+  #filter(product_id==13701, calendar_date <52) %>%
+    filter(product_id==13701) %>%
+  #arrange(calendar_date)
   arrange(as.Date(calendar_date))
 
 View(data_metro_eda)
 
 data_metro_eda <- data_metro_eda %>%
   ungroup() %>%
-  mutate(stock_levelD = dplyr::lead(stock_level,
-                                    order_by = as.Date(calendar_date), 
+  mutate(stock_levelD = dplyr::lag(stock_level,
+                                    order_by = as.Date(calendar_date),
+                                    # order_by = calendar_date,
                                     n=7,
-                                    defaul = stock_level )
+                                    defaul = stock_level ),
+         retail_priceD = dplyr::lag(retail_price,
+                                    order_by = as.Date(calendar_date),
+                                    # order_by = calendar_date,
+                                    n=7,
+                                    defaul = retail_price )
          )
 
-
+#rpivotTable::rpivotTable(data_metro_eda)
 plotgg(log(data_metro_eda$volume_sold+1), log(data_metro_eda$stock_level+1) )
 TSAnalysis(log(data_metro_eda$retail_price+1))
 
-ser.model      <- data_metro_eda$volume_sold
-ser.retprice   <- data_metro_eda$retail_price
+ser.model      <- ifelse(data_metro_eda$volume_sold<0,0,data_metro_eda$volume_sold)
+ser.retprice   <- data_metro_eda$retail_priceD
 ser.stock      <- data_metro_eda$stock_levelD
 ser.maxprice   <- data_metro_eda$mpdm1
 ser.cost       <- data_metro_eda$cdm1
 
-exo.vars <- data.frame(
-  cbind(price    = log(ser.retprice+1),
-        stock    = log(ser.stock+1),
-        maxprice = log(ser.maxprice+1),
-        cost     = log(ser.cost +1 ) )
-  )
 
+exo.vars <- data.frame(
+  cbind(price    = log(ifelse(ser.retprice<0, 0, ser.retprice)+1),
+        stock    = log(ifelse(ser.stock<0   , 0, ser.stock   )+1),
+        maxprice = log(ser.maxprice+1),
+        cost     = log(ifelse(ser.cost<0    , 0,ser.cost     )+1)
+        )
+  )
+data_metro_eda$mpdm1
+data_metro_eda$retail_price
+
+p<-ggplot(data_metro_eda)+
+  geom_line(aes(x = as.Date(calendar_date), y = mpdm1), group=1, colour=1)+
+  geom_line(aes(x = as.Date(calendar_date), y = retail_price), group=1, colour=2)+
+  geom_line(aes(x = as.Date(calendar_date), y = volume_sold), group=1, colour=3)+
+  theme_light()
+ggplotly(p)
+  
+
+
+forecast::Acf((log(ser.model+1)), 50)
+forecast::Pacf(log(ser.model+1), 50)
 
 model.exo <- forecast::Arima(
                 log(ser.model+1),
-                order = c(0,0,0),
+                #order = c(6,0,0), #works good for weekly timeset
+                order = c(2,1,0),
                 xreg = exo.vars,
                 include.drift = TRUE)
 
-model.exo
-forecast::Acf(model.exo$residuals, lag.max = 20)
-forecast::Pacf(model.exo$residuals, lag.max = 20)
+
+summary(model.exo)
+forecast::Acf(model.exo$residuals, lag.max = 50)
+forecast::Pacf(model.exo$residuals, lag.max = 50)
 
 summary(model.exo$residuals)
 plot(model.exo$residuals)
 
-ser.for <- forecast::forecast(model.exo, xreg = exo.vars, h =31)
+ser.for <- forecast::forecast(model.exo, xreg = exo.vars[1:30,])
+plot(ser.for)
 ori = exp(as.double(ser.for$x))-1
 fore = exp(as.double(ser.for$fitted))-1
 plotgg(ori,fore)
 
-mape(ori+1,fore+1)
+forecast::accuracy(model.exo)
+
+
+################### 3 #################################
+##################Aggregate model######################
+
+data_metro_eda <- data_metro %>%
+  mutate(calendar_date = woy) %>%
+  group_by(calendar_date) %>%
+  summarise(volume_sold = sum(volume_sold),
+            revenue = sum(revenue),
+            cost = sum(cost),
+            stock_level = sum(stock_level),
+            retail_price = sum(retail_price),
+            mpdm1 = sum(mpdm1),
+            cdm1  = sum(cdm1) )%>%
+  filter(calendar_date <52) %>%
+  arrange(calendar_date)
+
+
+View(data_metro_eda)
+
+data_metro_eda <- data_metro_eda %>%
+  ungroup() %>%
+  mutate(stock_levelD = dplyr::lag(stock_level,
+                                   order_by = calendar_date,
+                                   n=7,
+                                   defaul = stock_level ),
+         retail_priceD = dplyr::lag(retail_price,
+                                    order_by = calendar_date,
+                                    n=7,
+                                    defaul = retail_price )
+  )
+
+rpivotTable::rpivotTable(data_metro_eda)
+plotgg(log(data_metro_eda$volume_sold+1), log(data_metro_eda$stock_level+1) )
+TSAnalysis(log(data_metro_eda$retail_price+1))
+
+ser.model      <- ifelse(data_metro_eda$volume_sold<0,0,data_metro_eda$volume_sold)
+ser.retprice   <- data_metro_eda$retail_priceD
+ser.stock      <- data_metro_eda$stock_levelD
+ser.maxprice   <- data_metro_eda$mpdm1
+ser.cost       <- data_metro_eda$cdm1
+
+
+exo.vars <- data.frame(
+  cbind(price    = log(ifelse(ser.retprice<0, 0, ser.retprice)+1),
+        stock    = log(ifelse(ser.stock<0   , 0, ser.stock   )+1),
+        maxprice = log(ifelse(ser.maxprice<0, 0, ser.maxprice)+1),
+        cost     = log(ifelse(ser.cost<0    , 0,ser.cost     )+1)
+  )
+)
+
+
+forecast::Acf((log(ser.model+1)), 50)
+forecast::Pacf(log(ser.model+1), 50)
+
+model.exo <- forecast::Arima(
+  log(ser.model+1),
+  order = c(3,0,0),
+  xreg = exo.vars,
+  include.drift = TRUE)
+
+summary(model.exo)
+forecast::Acf(model.exo$residuals, lag.max = 50)
+forecast::Pacf(model.exo$residuals, lag.max = 50)
+
+summary(model.exo$residuals)
+plot(model.exo$residuals)
+
+ser.for <- forecast::forecast(model.exo, xreg = exo.vars[1:4,])
+plot(ser.for)
+ori = exp(as.double(ser.for$x))-1
+fore = exp(as.double(ser.for$fitted))-1
+plotgg(ori,fore)
+
+model.exo <- stats::arima(
+  log(ser.model+1),
+  order = c(3,0,0),
+  xreg = exo.vars)
+
+model.exo
+predict(model.exo, n.ahead = 1, newxreg = exo.vars[1:4,])
+forecast::accuracy(model.exo)
 
 
 
